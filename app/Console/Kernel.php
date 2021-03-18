cded8da3ece60403428c199b37855d9cecb61990
Daniel Morales <daniel85mg@gmail.com>
2021-03-17 21:02:35 -0300

    YEP, TASKS

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use App\Console\Commands\SendPingcommand;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -24,7 +26,23 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->everyMinute()
+            ->appendOutputTo(storage_path('logs/inspire_schedule.log'));
+
+        $schedule->command(SendPingcommand::class)
+            ->daily()
+            ->withoutOverlapping()
+            ->onOneServer();
+            //;
+            
+            //->evenInMaintenanceMode()
+            //->runInBackground();
+            //sendOutputTo
+            //appendOutputTo
+            //emailOutputTo
+            //emailOutputOnFailure
+
+            // sundays()->at('17:00')
     }
 
     /**
